a39d3f9e68242301ab6cc85de17fa0bd0be25e23
Jean Oliveira <63245911+jeanmicoliveira@users.noreply.github.com>
2020-08-31 11:38:18 -0300

    Update exercício01.py

--- exercício01.py
+++ exercício01.py
@@ -1,4 +1,4 @@
-"FAÇA UM PROGRAMA QUE OBTENHA UM NÚMERO INTEIRO DIGITADO PELO USUÁRIO, CALCULE E INFORME O DOBRO DESSE NÚMERO."
+"""FAÇA UM PROGRAMA QUE OBTENHA UM NÚMERO INTEIRO DIGITADO PELO USUÁRIO, CALCULE E INFORME O DOBRO DESSE NÚMERO."""
 
 valor = int(input('Digite um número inteiro para calcular o dobro do mesmo: '))
 print('O dobro é: ', valor*2)
