c19da71e18b1461b5a45817526834b05a43e0fba
Jean Oliveira <63245911+jeanmicoliveira@users.noreply.github.com>
2020-10-09 01:54:43 -0300

    Atividade3-PPSI

---
+++ Atividade03_PPSI/Exercício5.py
@@ -0,0 +1,20 @@
+"""
+Crie um programa que leia a altura de 10 pessoas. Ao final o programa deve informar o total de pessoas cadastradas, a quantidade de pessoas com altura inferior a 1,60 metros; a quantidade de pessoas com altura entre 1,60 metros e 1,80 metros e a quantidade de pessoas com altura superior a 1,80 metros.
+"""
+
+cadastros = inferior = entre = superior = 0
+
+for cadastros in range(10):
+    altura = float(input('Informe a altura: '))
+if altura < 1.6:
+    inferior += 1
+elif 1.8 >= altura >= 1.6:
+    entre += 1
+else:
+    superior +=1
+
+print(f'''
+Foram cadastradas {cadastros+1} pessoas
+Pessoas com altura menor de 1,6m: {inferior}
+Pessoas com altura entre 1,60m e 1,8m: {entre}
+Pessoas com altura superior a 1,8m: {superior}''')
