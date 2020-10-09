1b785960cbdb91598cc1c928f639ca07f137697d
Jean Oliveira <63245911+jeanmicoliveira@users.noreply.github.com>
2020-10-08 23:40:15 -0300

    Apostila1

---
+++ Apostila/Apostila1/apostila1-5.py
@@ -0,0 +1,19 @@
+"""Metodos para trabalhar com String"""
+
+a = "            Programando em Python"
+print(a.strip()) # retira os espaços.
+
+b = "Programando em Python"
+print(len(a)) # mostra o tamanho da string
+
+c = "Programando em Python"
+print(a.lower()) # tudo minúsculo
+
+d = "Programando em Python"
+print(a.upper()) # tudo maiúsculo
+
+e = "Programando em Python"
+print(a.replace("P", "C")) # substitui o P por C
+
+f = "Programando em Python"
+print(a.split(",")) # vai separar as palavras Programando e Python com uma virgula.
