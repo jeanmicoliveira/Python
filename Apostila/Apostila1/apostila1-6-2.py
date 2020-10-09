cc5f9be867e2e386e9dde5f80abef996868887c5
Jean Oliveira <63245911+jeanmicoliveira@users.noreply.github.com>
2020-10-09 00:00:29 -0300

    Apostila1

---
+++ Apostila/Apostila1/apostila1-6-2.py
@@ -0,0 +1,7 @@
+"""Exemplo 2"""
+
+print('Programa para somar dois valores inteiros.')
+v1 = int(input('Entre com o primeiro valor: '))
+v2 = int(input('Entre com o segundo valor: '))
+soma = (v1 + v2)
+print ('Resultado da soma:', soma)
---
+++ Apostila/Apostila1/apostila1-6.py
@@ -0,0 +1,12 @@
+"""Entrada de dados"""
+
+print("Digite seu nome")
+x = input()
+print("Muito bom te conhecer, " + x)
+
+"""
+Outra forma de fazer...
+"""
+y = input("Digite seu nome")
+print()
+print("Boa tarde, " + y)
